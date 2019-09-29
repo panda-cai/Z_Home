9a6784cf81c02074f28ea9a4cb2dc900e4d1810f
CaiShengDa <godezero@live.com>
2019-09-29 23:52:22 +0800

    product2

--- src/components/product/productClass.vue
+++ src/components/product/productClass.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="productClass">
-    <ul
-      class="left"
-      @touchmove="move"
-      :style="`transform: translateY(${end+len}px)`"
-      @touchstart="startTouch"
-      @touchend="leave" 
-      @click="changeClass"
-    >
-      <li data-i="0">123</li>
-      <li data-i="1">123</li>
-      <li data-i="2">123</li>
-      <li data-i="3">123</li>
-    </ul>
+    <div class="left">
+      <ul
+        @touchmove="move"
+        :style="`transform: translateY(${end+len}px)`"
+        @touchstart="startTouch"
+        @touchend="leave"
+        @click="changeClass"
+        ref="left"
+      >
+        <li data-i="0" :style="active=='0'?'color:#000':''" class="my-small">沙发</li>
+        <li data-i="1" :style="active=='1'?'color:#000':''" class="my-small">床</li>
+        <li data-i="2" :style="active=='2'?'color:#000':''" class="my-small">床品配件</li>
+        <li data-i="3" :style="active=='3'?'color:#000':''" class="my-small">椅凳</li>
+        <li data-i="4" :style="active=='4'?'color:#000':''" class="my-small">柜架</li>
+        <li data-i="5" :style="active=='5'?'color:#000':''" class="my-small">桌几</li>
+        <li data-i="6" :style="active=='6'?'color:#000':''" class="my-small">装饰</li>
+        <li data-i="7" :style="active=='7'?'color:#000':''" class="my-small">灯具</li>
+      </ul>
+    </div>
     <van-tabs v-model="active">
-      <van-tab title="标签 1"><h1>第0个</h1></van-tab>
-      <van-tab title="标签 2"><h1>第1个</h1></van-tab>
-      <van-tab title="标签 3"><h1>第2个</h1></van-tab>
-      <van-tab title="标签 4"><h1>第3个</h1></van-tab>
+      <van-tab>第0个</van-tab>
+      <van-tab>第1个</van-tab>
+      <van-tab>第2个</van-tab>
+      <van-tab>第3个</van-tab>
+      <van-tab>第3个</van-tab>
+      <van-tab>第3个</van-tab>
+      <van-tab>第3个</van-tab>
+      <van-tab>第3个</van-tab>
     </van-tabs>
   </div>
 </template>
@@ -25,7 +35,7 @@
 export default {
   data() {
     return {
-      active:0,
+      active: 0,
       start: 0,
       len: 0,
       add: 0,
@@ -33,24 +43,32 @@ export default {
     };
   },
   methods: {
-    changeClass(e){
-      if(e.target.nodeName=="LI"){
-        var index=e.target.getAttribute('data-i');
-        this.active=parseInt(index);
+    changeClass(e) {
+      if (e.target.nodeName == "LI") {
+        var index = e.target.getAttribute("data-i");
+        this.active = parseInt(index);
       }
       // console.log(this.active);
     },
     startTouch(e) {
       this.start = e.touches[0].pageY;
-      // console.log(e.touches[0].pageY);
     },
     move(e) {
       this.end = this.add;
       this.len = e.touches[0].pageY - this.start;
-      // console.log(e.touches[0].pageY);
-      // console.log(1);
     },
     leave(e) {
+      var height=this.$refs.left.offsetHeight-window.innerHeight+104;
+      // console.log(height);
+      if (this.end + this.len > 0) {
+        // console.log("c大于0了");
+        this.$refs.left.style = "transform: translateY(0px)";
+        return;
+      }
+      else if(this.end + this.len <-height){
+        this.$refs.left.style = `transform: translateY(-${height}px)`;
+        return;
+      }
       this.add += this.len;
     }
   }
@@ -68,17 +86,16 @@ export default {
   /* overflow: scroll; */
   position: fixed;
   left: 0;
-  width: 20%;
-  border-right: 1px solid #000;
+  width: 26%;
+  border-right: 1px solid #efefef;
   height: 900px;
-  background: #f5f5f5;
-  z-index:1;
+  background: #fff;
+  z-index: 1;
 }
 .left li {
-  padding: 15px;
-  border-bottom: 1px solid #999;
+  padding: 1.3rem 0;
 }
-.van-tabs--line >>> .van-tabs__wrap{
-  display:none;
+.van-tabs--line >>> .van-tabs__wrap {
+  display: none;
 }
 </style>
